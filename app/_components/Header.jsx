4daf27fc96d0b113db4ad41623d20e48587c13ae
ZakDeveloperAI <belfakhirzakaria@gmail.com>
2024-08-12 16:08:37 +0200

    continued authentication and little logo fix

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
   return (
     <div className='p-5 shadow-sm flex justify-between'>
         <div className='flex items-center  gap-8'>
-            <Image src='../logo.svg' alt="logo" width={200} height={200}/>
+            <Image src='logo.svg' alt="logo" width={200} height={200}/>
             <div className='md:flex items-center gap-6 hidden'>
                 <h2 className=' hover:scale-105 hover:text-primary cursor-pointer'>Home</h2>
                 <h2 className=' hover:scale-105 hover:text-primary cursor-pointer'>Services</h2>
