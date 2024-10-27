14aa037535af0648a75947e8487744982bc2946a
ZakDeveloperAI <belfakhirzakaria@gmail.com>
2024-10-27 18:29:15 +0100

    solved query issue, and intialized details page

--- app/(routes)/details/[businessId]/page.jsx
+++ app/(routes)/details/[businessId]/page.jsx
@@ -1,21 +1,42 @@
 "use client"
+import GlobalApi from '@/app/_services/GlobalApi';
 import { signIn, useSession } from 'next-auth/react'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-function BusinessDetail() {
+function BusinessDetail({params}) {
 
     const {data,status} = useSession();
+    const [business, setBusiness] = useState([]);
 
-    if (status == "loading") {
+
+    useEffect(()=>{
+        params&&getbusinessById();//esego solo quando params non è null
+    },[params])//le quadre dicono che ogni volta che params subisce una variazione esegue il metodo
+
+    useEffect(()=>{
+        checkUserAuth(); //le quadre servono cosi ripete solo una volta o quando il componente viene montato o smonatato
+    },[])
+
+    const  getbusinessById=()=>{
+      GlobalApi.getBusinessById(params.businessId).then(resp=>{
+          setBusiness(resp.businessList);
+      }) //businessId deriva dalla cratella variabile [businessId]
+  }
+
+    const checkUserAuth=()=>{
+      if (status == "loading") {
         return <div>Loading...</div>
     }
 
     if (status == "unauthenticated") {
         signIn('descope');
     }
+    }
+
   return status == "authenticated" && (
     <div>BusinessDetail</div>
   )
 }
 
 export default BusinessDetail
+
--- app/_services/GlobalApi.js
+++ app/_services/GlobalApi.js
@@ -70,8 +70,35 @@ const getBusinessByCategory = async (category) => {
     return result
 }
 
+
+const getBusinessById = async (id) => {
+    const query = gql`
+        query GetBusinessById {
+            businessList(where: { id: "`+id+`" }) {
+                about
+                address
+                category {
+                    name
+                }
+                contactPerson
+                email
+                id
+                name
+                images {
+                    url
+                }
+            }
+        }
+    `
+    const result = await request(MASTER_URL, query)
+    return result
+}
+
+
+
 export default {
     getCategory,
     getAllBusinessList,
-    getBusinessByCategory
+    getBusinessByCategory,
+    getBusinessById
 }
