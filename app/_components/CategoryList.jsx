f861483f35736436a4a80110a4829999d97c5a51
ZakDeveloperAI <belfakhirzakaria@gmail.com>
2025-03-08 14:45:29 +0100

    minor fix

--- app/_components/CategoryList.jsx
+++ app/_components/CategoryList.jsx
@@ -4,12 +4,12 @@ import React from 'react'
 
 function CategoryList({categoryList}) {
   return (
-    <div className='mx-4 md:mx-22 lg:mx-52 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4'>
+    <div className='mx-4 md:mx-22 lg:mx-52 grid grid-cols-3 md:grid-cols-3 lg:grid-cols-6 gap-4'>
       {categoryList.length>0?categoryList.map((category,index)=>(
-        <Link href={'/search/'+category.name}  key={index} className={`flex flex-col items-center justify-center gap-2 
-          bg-purple-100 p-5 rounded-lg cursor-pointer hover:scale-110 transition-all ease-in-out]
+        <Link href={'/searchCategory/'+category.name}  key={index} className={`flex flex-col items-center justify-center gap-2 
+          bg-purple-100 p-4 rounded-lg cursor-pointer hover:scale-110 transition-all ease-in-out]
           `}>
-          <Image src={category.icon.url} alt='icon' width={35} height={35}/>
+          <Image src={category.icon.url} alt='icon' width={40} height={40}/>
           <h2 className='text-primary'>{category.name}</h2>
         </Link>
       )):
